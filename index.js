3f9988632afd5418ef424e2ea75314cc02314a8d
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-06-15 23:39:13 +0300

    commit tree 79dc34a601c75c1f87f39258bee64813d508cd91

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+let num1 = 62;
+let num2 = 1;
+let multiply = num1 * num2;
+
+//generate a random integer greater than 0
+// let random = Math.ceil(Math.random());
+let random = math.random() * 10;
+
+//calculate remainder that is equal to 4
+let num3 = 9;
+let num4 = 5;
+let mod = num3 % num4;
+
+//return 20 as the highest number in the set
+let max = Math.max(2, 4, 5, 20, 17, 13);
